f5733acf466b44cc06a1104ab7b0595273c7ac3e
Matteo Muscas <thatman.j@gmail.com>
2019-07-12 08:07:05 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
+<<<<<<< HEAD
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
@@ -11,4 +12,7 @@ class User < ActiveRecord::Base
        user.password = Devise.friendly_token[0,20]
      end
    end
+=======
+         :recoverable, :rememberable, :trackable, :validatable
+>>>>>>> 5d3f30bc8860185b76cbd955c440283bd6d6f845
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   #devise_for :users
+=======
+  devise_for :users
+>>>>>>> 5d3f30bc8860185b76cbd955c440283bd6d6f845
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#home'
+<<<<<<< HEAD
   get '/about' => 'welcome#about'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+=======
+>>>>>>> 5d3f30bc8860185b76cbd955c440283bd6d6f845
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
