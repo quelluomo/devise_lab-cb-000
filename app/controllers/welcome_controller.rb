a0d10f6e864311c37bd32be52e09b1da4ccf3372
Matteo Muscas <thatman.j@gmail.com>
2019-07-12 08:21:25 +0000

    Automatically backed up by Learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,8 @@ class WelcomeController < ApplicationController
   def home
   end
 
-<<<<<<< HEAD
   def about
     render :text => "About the Website"
   end
 
-=======
->>>>>>> 5d3f30bc8860185b76cbd955c440283bd6d6f845
 end
